d1f86aa1f00629df4899741f71f74e8103b53818
Denis <grichenko122@gmail.com>
2019-10-15 13:33:35 +0300

    added basic components

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Footer.css';
+
+class Footer extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <footer>
+        <p>Footer</p>
+      </footer>
+    )
+  }
+}
+
+export default Footer;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Main.css';
+
+class Main extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <main>
+        <p>Main content</p>
+      </main>
+    )
+  }
+}
+
+export default Main;
