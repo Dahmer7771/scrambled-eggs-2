e2d1882c09e9dc126ae06e156354b98d7f3429f7
Denis <grichenko122@gmail.com>
2019-10-15 13:16:44 +0300

    test commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.css';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Hello</p>
+      </div>
+    )
+  }
+}
+
+export default Header;
