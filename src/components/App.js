09614a9f22f193d3069ddbd65232a540cc18ce36
valeravarchak <valeraavarchak@gmail.com>
2019-10-17 01:27:50 +0300

    1. Added styles into Recipes.css 2. Added className and blocks into RecipesSection.js 3. Deleted unnecessary components into App.js

--- src/components/App.js
+++ src/components/App.js
@@ -14,8 +14,8 @@ function App() {
     <div className="App">
       <Header />
       <CustomHR />
-      <SelectBox />
-      <CustomHR />
+
+
       <RecipesSection />
       <Main />
       <Footer />
--- src/components/RecipesSection/RecipesSection.js
+++ src/components/RecipesSection/RecipesSection.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import './RecipesSection.css';
+
 import RecipesSectionItem from '../RecipesSectionItem/RecipesSectionItem';
 
 class RecipesSection extends Component {
@@ -11,6 +13,12 @@ class RecipesSection extends Component {
     render() { 
         return (
             <section className="recipes-section container">
+                <h3>Рецепты</h3>
+                <div className="row justify-content-center">
+                        <div className="recommended col-2">Рекомендуем</div>
+                        <div className="new-recipes col-2">Новые рецепты</div>
+                        <div className="popular col-2">Популярные</div>
+                </div>
                 <div className="row">
                     <RecipesSectionItem />
                     <RecipesSectionItem />
