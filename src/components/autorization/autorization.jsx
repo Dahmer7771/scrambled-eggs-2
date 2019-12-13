110c477f453ec030968be6a278a5d3ed6ce94808
valeravarchak <valeraavarchak@gmail.com>
2019-12-13 05:10:12 +0200

    fix autorization

--- src/components/autorization/autorization.jsx
+++ src/components/autorization/autorization.jsx
@@ -51,17 +51,17 @@ class Autorization extends Component {
                         <button type="button" className="btn btn-light" onClick={this.showLoginForm}>Вход</button>
                         <button type="button" className="btn btn-light" onClick={this.showRegistrationForm}>Регистрация</button>
                     </div>
-                    <form className={`login col-md-10 offset-md-1 ${loginVisibility}`}>
+                    <form className={`login col-sm-12 col-md-12 col-lg-12 col-xl-12 offset-md-1 ${loginVisibility}`}>
                         <div className="form-group row">
                             <label htmlFor="inputlogin3" className="col-sm-2 col-form-label">Login</label>
-                            <div className="col-sm-10">
-                                <input type="login" className="form-control" id="inputEmail3" />
+                            <div className="col-sm-10 col-md-10 col-lg-10 col-xl-10">
+                                <input type="login" className="form-control" id="inputLogin" />
                             </div>
                         </div>
                         <div className="form-group row">
                             <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
-                            <div className="col-sm-10">
-                                <input type="password" className="form-control" id="inputPassword3" />
+                            <div className="col-sm-10 col-md-10 col-lg-10 col-xl-10">
+                                <input type="password" className="form-control" id="inputPasswordLogin" />
                             </div>
                         </div>
                         <div className="form-group row">
@@ -71,23 +71,23 @@ class Autorization extends Component {
                         </div>
                     </form>
 
-                    <form className={`registration col-md-10 offset-md-1 ${registrationVisibility}`}>
+                    <form className={`registration col-sm-12 col-md-12 col-lg-12 col-xl-12 offset-md-1 ${registrationVisibility}`}>
                         <div className="form-group row">
                             <label htmlFor="inputlogin3" className="col-sm-2 col-form-label">Login</label>
-                            <div className="col-sm-10">
-                                <input type="login" className="form-control" id="inputEmail3" />
+                            <div className="col-sm-10 col-md-10 col-lg-10 col-xl-10">
+                                <input type="login" className="form-control" id="inputRegistration" />
                             </div>
                         </div>
                         <div className="form-group row">
                             <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
-                            <div className="col-sm-10">
-                                <input type="email" className="form-control" id="inputEmail3" />
+                            <div className="col-sm-10 col-md-10 col-lg-10 col-xl-10">
+                                <input type="email" className="form-control" id="inputEmail" />
                             </div>
                         </div>
                         <div className="form-group row">
                             <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
-                            <div className="col-sm-10">
-                                <input type="password" className="form-control" id="inputPassword3" />
+                            <div className="col-sm-10 col-md-10 col-lg-10 col-xl-10">
+                                <input type="password" className="form-control" id="inputPasswordRegistration" />
                             </div>
                         </div>
                         <div className="form-group row">
@@ -96,7 +96,6 @@ class Autorization extends Component {
                             </div>
                         </div>
                     </form>
-
                 </div>
             </div>
         );
