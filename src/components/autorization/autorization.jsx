7b018415fb596c0a369660168e6c239bfc0b0dcb
dahmer7771 <Grichenko122@gmail.com>
2019-12-13 07:46:13 +0300

    changed styles of autorization

--- src/components/autorization/autorization.jsx
+++ src/components/autorization/autorization.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./autorization.css";
+import formValidation from "../../helpers/validation";
 
 class Autorization extends Component {
     constructor(props) {
@@ -7,11 +8,154 @@ class Autorization extends Component {
         this.state = {
             loginVisibility: "",
             registrationVisibility: "styleHidden",
+
+            isLoginActive: true,
+            isRegistrationActive: false,
+
+            inputValues: {
+                usernameLogin: "",
+                passwordLogin: "",
+                usernameRegistration: "",
+                emailRegistration: "",
+                passwordRegistration: "",
+                passwordConfirmRegistration: "",
+            },
+
+            errorMessage: {
+                usernameLogin: "",
+                passwordLogin: "",
+                usernameRegistration: "",
+                emailRegistration: "",
+                passwordRegistration: "",
+                passwordConfirmRegistration: "",
+            },
+
+            errorVisibility: {
+                usernameLogin: {
+                    visibility: "hidden",
+                },
+                passwordLogin: {
+                    visibility: "hidden",
+                },
+                usernameRegistration: {
+                    visibility: "hidden",
+                },
+                emailRegistration: {
+                    visibility: "hidden",
+                },
+                passwordRegistration: {
+                    visibility: "hidden",
+                },
+                passwordConfirmRegistration: {
+                    visibility: "hidden",
+                },
+            },
+
+            errorBorder: {
+                usernameLogin: {
+                    borderBottom: "2px solid rgba(255,255,255,0.24)",
+                },
+                passwordLogin: {
+                    borderBottom: "2px solid rgba(255,255,255,0.24)",
+                },
+                usernameRegistration: {
+                    borderBottom: "2px solid rgba(255,255,255,0.24)",
+                },
+                emailRegistration: {
+                    borderBottom: "2px solid rgba(255,255,255,0.24)",
+                },
+                passwordRegistration: {
+                    borderBottom: "2px solid rgba(255,255,255,0.24)",
+                },
+                passwordConfirmRegistration: {
+                    borderBottom: "2px solid rgba(255,255,255,0.24)",
+                },
+            },
         };
 
         this.showLoginForm = this.showLoginForm.bind(this);
         this.showRegistrationForm = this.showRegistrationForm.bind(this);
     }
+
+    handleFormInput = (e) => {
+        const {
+            name,
+            value,
+        } = e.target;
+        const {
+            errorMessage,
+            errorBorder,
+            errorVisibility,
+            inputValues,
+        } = this.state;
+
+        const isValid = formValidation(name, value);
+
+        if (isValid.status === "success") {
+            if (name === "passwordConfirmRegistration" && value !== inputValues.passwordRegistration) {
+                this.setState({
+                    errorMessage: {
+                        ...errorMessage,
+                        [name]: "passwords does not match",
+                    },
+                    errorVisibility: {
+                        ...errorVisibility,
+                        [name]: {
+                            visibility: "visible",
+                        },
+                    },
+                    errorBorder: {
+                        ...errorBorder,
+                        [name]: {
+                            borderBottom: "2px solid red",
+                        },
+                    },
+                });
+                return;
+            }
+            this.setState({
+                inputValues: {
+                    ...inputValues,
+                    [name]: value,
+                },
+                errorMessage: {
+                    ...errorMessage,
+                    [name]: "",
+                },
+                errorVisibility: {
+                    ...errorVisibility,
+                    [name]: {
+                        visibility: "hidden",
+                    },
+                },
+                errorBorder: {
+                    ...errorBorder,
+                    [name]: {
+                        borderBottom: "2px solid rgba(255,255,255,0.24)",
+                    },
+                },
+            });
+        } else {
+            this.setState({
+                errorMessage: {
+                    ...errorMessage,
+                    [name]: isValid.error,
+                },
+                errorVisibility: {
+                    ...errorVisibility,
+                    [name]: {
+                        visibility: "visible",
+                    },
+                },
+                errorBorder: {
+                    ...errorBorder,
+                    [name]: {
+                        borderBottom: "2px solid red",
+                    },
+                },
+            });
+        }
+    };
 
     showLoginForm = () => {
         const {
@@ -22,6 +166,8 @@ class Autorization extends Component {
             this.setState({
                 registrationVisibility: "styleHidden",
                 loginVisibility: "",
+                isLoginActive: true,
+                isRegistrationActive: false,
             });
         }
     };
@@ -35,6 +181,8 @@ class Autorization extends Component {
             this.setState({
                 loginVisibility: "styleHidden",
                 registrationVisibility: "",
+                isLoginActive: false,
+                isRegistrationActive: true,
             });
         }
     };
@@ -43,30 +191,48 @@ class Autorization extends Component {
         const {
             loginVisibility,
             registrationVisibility,
+            errorMessage,
+            errorVisibility,
+            isLoginActive,
+            isRegistrationActive,
         } = this.state;
+
+        const loginToggle = isLoginActive ? "btn-primary" : "btn-light";
+        const registrationToggle = isRegistrationActive ? "btn-primary" : "btn-light";
+
         return (
             <div className="container">
                 <div className="autorization-container row">
                     <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12">
                         <div className="btn-group autorization-buttons" role="group" aria-label="Basic example">
-                            <button type="button" className="btn btn-light" onClick={this.showLoginForm}>Вход</button>
-                            <button type="button" className="btn btn-light" onClick={this.showRegistrationForm}>Регистрация</button>
+                            <button type="button" className={`btn ${loginToggle}`} onClick={this.showLoginForm}>Вход</button>
+                            <button type="button" className={`btn ${registrationToggle}`} onClick={this.showRegistrationForm}>Регистрация</button>
                         </div>
                         <form className={`login col-sm-12 col-md-12 col-lg-12 col-xl-12 ${loginVisibility}`}>
                             <div className="form-group row">
-                                <label htmlFor="inputlogin3" className="col-sm-2 col-form-label">Login</label>
-                                <div className="col-sm-10 col-md-10 col-lg-10 col-xl-10">
-                                    <input type="login" className="form-field form-control" id="inputLogin" />
+                                <label htmlFor="inputLogin" className="col-sm-2 col-md-3 col-lg-2 col-form-label">Login</label>
+                                <div className="col-sm-12 col-md-9 col-lg-10 col-xl-10">
+                                    <input type="login" name="usernameLogin" className="form-field form-control" id="inputLogin" onChange={this.handleFormInput} />
+                                </div>
+                                <div className="col-sm-12 offset-lg-2 col-md-9 offset-md-3 col-lg-10 col-xl-10">
+                                    <div className="error-message" style={errorVisibility.usernameLogin}>
+                                        {errorMessage.usernameLogin}
+                                    </div>
                                 </div>
                             </div>
                             <div className="form-group row">
-                                <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
-                                <div className="col-sm-10 col-md-10 col-lg-10 col-xl-10">
-                                    <input type="password" className="form-field form-control" id="inputPasswordLogin" />
+                                <label htmlFor="inputPasswordLogin" className="col-sm-2 col-md-3 col-lg-2 col-form-label">Password</label>
+                                <div className="col-sm-12 col-md-9 col-lg-10 col-xl-10">
+                                    <input type="password" name="passwordLogin" className="form-field form-control" id="inputPasswordLogin" onChange={this.handleFormInput} />
+                                </div>
+                                <div className="col-sm-12 offset-lg-2 col-md-9 offset-md-3 col-lg-10 col-xl-10">
+                                    <div className="error-message" style={errorVisibility.passwordLogin}>
+                                        {errorMessage.passwordLogin}
+                                    </div>
                                 </div>
                             </div>
                             <div className="form-group row">
-                                <div className="col-sm-10">
+                                <div className="col-sm-12">
                                     <button type="submit" className="btn btn-primary">Sign in</button>
                                 </div>
                             </div>
@@ -74,26 +240,52 @@ class Autorization extends Component {
 
                         <form className={`registration col-sm-12 col-md-12 col-lg-12 col-xl-12 ${registrationVisibility}`}>
                             <div className="form-group row">
-                                <label htmlFor="inputlogin3" className="col-sm-2 col-form-label">Login</label>
-                                <div className="col-sm-10 col-md-10 col-lg-10 col-xl-10">
-                                    <input type="login" className="form-field form-control" id="inputRegistration" />
+                                <label htmlFor="inputRegistration" className="col-sm-2 col-md-3 col-lg-2 col-form-label">Login</label>
+                                <div className="col-sm-12 col-md-9 col-lg-10 col-xl-10">
+                                    <input type="login" name="usernameRegistration" className="form-field form-control" id="inputRegistration" onChange={this.handleFormInput} />
+                                </div>
+                                <div className="col-sm-12 offset-lg-2 col-md-9 offset-md-3 col-lg-10 col-xl-10">
+                                    <div className="error-message" style={errorVisibility.usernameRegistration}>
+                                        {errorMessage.usernameRegistration}
+                                    </div>
                                 </div>
                             </div>
                             <div className="form-group row">
-                                <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
-                                <div className="col-sm-10 col-md-10 0col-lg-10 col-xl-10">
-                                    <input type="email" className="form-field form-control" id="inputEmail" />
+                                <label htmlFor="inputEmail" className="col-sm-2 col-md-3 col-lg-2 col-form-label">Email</label>
+                                <div className="col-sm-12 col-md-9 col-lg-10 col-xl-10">
+                                    <input type="email" name="emailRegistration" className="form-field form-control" id="inputEmail" onChange={this.handleFormInput} />
+                                </div>
+                                <div className="col-sm-12 offset-lg-2 col-md-9 offset-md-3 col-lg-10 col-xl-10">
+                                    <div className="error-message" style={errorVisibility.emailRegistration}>
+                                        {errorMessage.emailRegistration}
+                                    </div>
                                 </div>
                             </div>
                             <div className="form-group row">
-                                <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
-                                <div className="col-sm-10 col-md-10 col-lg-10 col-xl-10">
-                                    <input type="password" className="form-field form-control" id="inputPasswordRegistration" />
+                                <label htmlFor="inputPasswordRegistration" className="col-sm-2 col-md-3 col-lg-2 col-form-label">Password</label>
+                                <div className="col-sm-12 col-md-9 col-lg-10 col-xl-10">
+                                    <input type="password" name="passwordRegistration" className="form-field form-control" id="inputPasswordRegistration" onChange={this.handleFormInput} />
+                                </div>
+                                <div className="col-sm-12 offset-lg-2 col-md-9 offset-md-3 col-lg-10 col-xl-10">
+                                    <div className="error-message" style={errorVisibility.passwordRegistration}>
+                                        {errorMessage.passwordRegistration}
+                                    </div>
                                 </div>
                             </div>
                             <div className="form-group row">
-                                <div className="col-sm-10">
-                                    <button type="submit" className="btn btn-primary">Registration</button>
+                                <label htmlFor="inputPasswordConfirmRegistration" className="col-sm-2 col-md-3 col-lg-2 col-form-label">Confirm password</label>
+                                <div className="col-sm-12 col-md-9 col-lg-10 col-xl-10">
+                                    <input type="password" name="passwordRegistration" className="form-field form-control" id="inputPasswordConfirmRegistration" onChange={this.handleFormInput} />
+                                </div>
+                                <div className="col-sm-12 offset-lg-2 col-md-9 offset-md-3 col-lg-10 col-xl-10">
+                                    <div className="error-message" style={errorVisibility.passwordConfirmRegistration}>
+                                        {errorMessage.passwordConfirmRegistration}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="form-group row">
+                                <div className="col-sm-12">
+                                    <button type="submit" className="btn btn-primary">Sign up</button>
                                 </div>
                             </div>
                         </form>
