9ad53f12020d167ee7b28ef4e9f85ec55d991516
valeravarchak <valeraavarchak@gmail.com>
2019-12-17 00:09:18 +0200

    created  component cusers

--- src/components/users/users.jsx
+++ src/components/users/users.jsx
@@ -3,33 +3,7 @@ import "./users.css";
 import withContext from "../hoc-helpers/withContext";
 
 class Users extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            recipes: null,
-        };
-    }
-
-    componentDidMount() {
-        const {
-            getAll,
-        } = this.props;
-
-        const recipes = getAll();
-
-        this.setState({
-            recipes,
-        });
-    }
-
-
     render() {
-        const {
-            recipes,
-        } = this.state;
-
-        if (!recipes) return <p>error</p>;
-
         return (
             <div className="container">
                 <table className="table users-table">
@@ -41,19 +15,15 @@ class Users extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {
-                            recipes.map(({ id, name }) => (
-                                <tr key={id}>
-                                    <th scope="row">{id}</th>
-                                    <td>{name}</td>
-                                    <td>
-                                        <button type="submit" className="btn btn-light">
-                                            <i className="material-icons">delete_forever</i>
-                                        </button>
-                                    </td>
-                                </tr>
-                            ))
-                        }
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Name</td>
+                            <td>
+                                <button type="submit" className="btn btn-light">
+                                    <i className="material-icons">delete_forever</i>
+                                </button>
+                            </td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
