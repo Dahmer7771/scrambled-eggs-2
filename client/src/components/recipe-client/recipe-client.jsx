ec062515a59dc7367e4041d2bfdca9caf8eb77a1
valeravarchak <valeraavarchak@gmail.com>
2019-12-18 01:03:04 +0200

    added style recipe-client

--- client/src/components/recipe-client/recipe-client.jsx
+++ client/src/components/recipe-client/recipe-client.jsx
@@ -1,22 +1,25 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "./recipe-client.css"
 
 class RecipeClient extends Component {
     render() {
         return (
             <div className="container">
-                <table className="table">
+                <table className="table user-recipe">
                     <thead className="thead-light">
                         <tr>
                             <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">First</th>
+                            <th scope="col">Название рецепта</th>
+                            <th scope="col">Дата</th>
+                            <th scope="col">Изменить/Удалить</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
                             <th scope="row">1</th>
                             <td>Mark</td>
+                            <td>без 5минут</td>
                             <td>
                                 <Link to="/change">
                                     <button type="button" className="btn btn-secondary material-icons">
