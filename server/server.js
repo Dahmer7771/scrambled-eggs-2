ab78af3022aa6829896d6c26a427f5c09b679090
Nik <nikitaholub1999@gmail.com>
2019-12-18 01:42:27 +0200

    add ingredients 2

--- server/server.js
+++ server/server.js
@@ -66,192 +66,63 @@ const upload = multer({
   fileFilter: fileFilter
 });
 
-
 app.post('/api/recipe/create', upload.single('image'), (req, res) => {
 
-  //ПОПЫТКА 4
+  let recipe;
 
-  // let recipe;
+  if(req.file){
+    recipe = new Recipe({
+    name: req.body.name,
+    description: req.body.description,
+    category: req.body.category,
+    ingredient: req.body.ingredient,
+    mark: req.body.mark,
+    createdBy: req.body.createdBy, 
+    image: req.file.path
+  });}
+  else {
+    recipe = new Recipe(req.body);
+  }
 
-  // if(req.file){
-  //   recipe = new Recipe({
-  //   name: req.body.name,
-  //   description: req.body.description,
-  //   category: req.body.category,
-  //   ingredient: req.body.ingredient,
-  //   mark: req.body.mark,
-  //   createdBy: req.body.createdBy, 
-  //   image: req.file.path
-  // });}
-  // else {
-  //   recipe = new Recipe(req.body);
-  // }
-
-  // recipe
-  //   .save()
-  //   .then(result => {
-  //     res.status(201).json({
-  //       message: "Created product successfully"
-  //     });
-  //   })
-  //   .catch(err => {
-  //   console.log(err);
-  //   res.status(500).json({
-  //     error: err
-  //   });
-  //   });
-
-    // ПОПЫТКА 5
-    let recipe;
-
-    if(req.file){
-      recipe = new Recipe({
-      name: req.body.name,
-      description: req.body.description,
-      category: req.body.category,
-      ingredient: req.body.ingredient,
-      mark: req.body.mark,
-      createdBy: req.body.createdBy, 
-      image: req.file.path
-    });}
-    else {
-      recipe = new Recipe(req.body);
-    }
-
-    recipe
-      .save()
-      .then( () => {
-        let ingredients = req.body.ingredient;
-    
-        for (let i = 0; i < ingredients.length; i++){
-          let ingredient = new Ingredient({name: ingredients[i], recipe: recipe._id})
-          // console.log(ingredient);
-
-          Ingredient.find({name: ingredients[i]}, function (err, docs) {
-            if (docs.length){
-
-              Ingredient.findOneAndUpdate({
-                name: `${ingredients[i]}`}, 
-                {$push: {recipe: recipe._id}}, 
-                {new: true, useFindAndModify: false})
-              .populate('recipe')
-              .exec((err) => {
-                if (err) {
-                  return res.status(400).json({
-                    error: errorHandler.getErrorMessage(err)
-                  })
-                }
-              })
-            }
-            else{
-              ingredient.save();
-            }
-          });
-        }
-      })
-      .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
-      });
-    
-    // console.log(recipe._id)
-    
-    // let ingredients = req.body.ingredient;
-    
-    // for (let i = 0; i <= ingredients.length; i++){
-    //   let ingredient = new Ingredient({name: ingredients[i]}, {recipe: recipe._id})
-    //   console.log(ingredient);
-    //   if (Ingredient.find({name: `${ingredient[i]}`})){
-    //     Ingredient.findOneAndUpdate({
-    //       name: `${ingredient[i]}`}, 
-    //       {$push: {recipe: recipe._id}}, 
-    //       {new: true, useFindAndModify: false})
-    //     // .populate('recipe')
-    //     .exec((err) => {
-    //       if (err) {
-    //         return res.status(400).json({
-    //           error: errorHandler.getErrorMessage(err)
-    //         })
-    //       }
-    //     })
-    //   }
-    //   else ingredient.save().populate('recipe');
-    // }
+  recipe
+    .save()
+    .then( () => {
+      let ingredients = req.body.ingredient;
   
-  //ПЛОХОЙ ВАРИАНТ
-  // const ingredients = req.body.ingredient;
+      for (let i = 0; i < ingredients.length; i++){
+        let ingredient = new Ingredient({name: ingredients[i], recipe: recipe._id})
 
-  // for (let i = 0; i <= ingredients.length; i++){
-  //   let ingredient = new Ingredient({name: ingredients[i]});
-  //   if (Ingredient.find({name: `${ingredient[i]}`})) continue;
-  //   ingredient.save();
-  // }
+        Ingredient.find({name: ingredients[i]}, function (err, docs) {
+          if (docs.length){
 
-  // recipe = new Recipe(req.body);
-
-  // recipe.save((err,doc)=>{
-  //     if(err) return res.json({success:false,err});
-  //     res.status(200).json({
-  //         success: true,
-  //         article: doc
-  //     })
-  // })
-
-  //ВТОРОЙ ВАРИАНТ
-  
-  // const recipe = new Recipe(req.body);
-
-  // if (Recipe.find({name: `${recipe.name}`})) {
-  //   //TODO
-  // }
-  // else {
-  //   await recipe.save();  
-  // }
-
-  // let flag = true;
-  // await Recipe.find({name: `${recipe.name}`}).then((data) => {
-  //   console.log(data)
-  //   if (data.toString === []) { 
-  //     //TODO
-  //     flag = false; 
-  //   }
-  //   else {
-      
-  //   }
-  // })
-  // console.log(flag);
-  // if (flag) {
-  //   await recipe.save();
-  // }
-
-
-  // let recipeData = {};
-
-  // await Recipe.findOne({name: recipe.name}).then((data) => {
-  //   // console.log(data);
-  //   recipeData = data;
-  //   console.log(recipeData);
-  // });
-  
-  // let recipeID = recipeData._id;
-  // console.log(recipeID);
-  
-
-  // for (let i = 0; i <= ingredients.length; i++){
-  //   let ingredient = new Ingredient({name: ingredients[i]});
-  //   if (Ingredient.find({name: `${ingredient[i]}`})) {
-  //     Ingredient.findOneAndUpdate(ingredientn.name, {$push: {recipe: recipe.name}}, {new: true}).
-  //     populate('recipe');
-  //     continue;
-  //   }
-  //   else {
-  //     ingredient.recipe = recipeID;
-  //     ingredient.save();
-  //   }
-  // }
+            Ingredient.findOneAndUpdate({
+              name: `${ingredients[i]}`}, 
+              {$push: {recipe: recipe._id}}, 
+              {new: true, useFindAndModify: false})
+            .populate('recipe')
+            .exec((err) => {
+              if (err) {
+                return res.status(400).json({
+                  error: errorHandler.getErrorMessage(err)
+                })
+              }
+            })
+          }
+          else{
+            ingredient.save();
+          }
+        });
+      }
+    })
+    .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error: err
+    });
+    }); 
 })
+
+
 
 //GET RECIPES
 // /articles?sortBy=created&order=desc&limit=100
@@ -261,10 +132,12 @@ app.get('/api/recipes/articles', (req, res) => {
   let order = req.query.order ? req.query.order : 'asc';
   let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
   let limit = req.query.limit ? parseInt(req.query.limit) : 100;
+  let skip = parseInt(req.body.skip);
 
   Recipe.
   find().
   sort([[sortBy,order]]).
+  skip(skip).
   limit(limit).
   exec((err,articles)=>{
     if(err) return res.status(400).send(err);
