042e8665938a587ce7eb58fb2b53be8ef07287b9
Nik <nikitaholub1999@gmail.com>
2019-12-24 19:29:36 +0200

    new build

---
+++ client/build/precache-manifest.701c7e2d3974cc6026f7259cc90e2ef2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8969b64832ce7dd98a57001ccb1c10a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b552e211a29f7b0fafb6",
+    "url": "/static/css/2.3d86be27.chunk.css"
+  },
+  {
+    "revision": "67021c32d350c761a907",
+    "url": "/static/css/main.ad3c1717.chunk.css"
+  },
+  {
+    "revision": "b552e211a29f7b0fafb6",
+    "url": "/static/js/2.80405585.chunk.js"
+  },
+  {
+    "revision": "d35bb95acbf4d4ae17c99face9498f03",
+    "url": "/static/js/2.80405585.chunk.js.LICENSE"
+  },
+  {
+    "revision": "67021c32d350c761a907",
+    "url": "/static/js/main.7ad72597.chunk.js"
+  },
+  {
+    "revision": "42563c0706ebf2262a23",
+    "url": "/static/js/runtime-main.e6d8a004.js"
+  }
+]);
