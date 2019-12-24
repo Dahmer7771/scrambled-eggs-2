bf810db89399eff219770b61d0924fcfd7dcdcab
Nik <nikitaholub1999@gmail.com>
2019-12-24 08:25:52 +0200

    dns

--- server/server.js
+++ server/server.js
@@ -49,7 +49,7 @@ app.use("", userRoutes);
 
 app.get('/', (req, res) => {
   res.status(200);
-  res.send('<p>some html</p>');
+  res.send('<p>Манюсенькие проблемы с клиентской частью, скоро всё будет</p>');
 })
 
 app.listen(PORT, DNS, () => {
