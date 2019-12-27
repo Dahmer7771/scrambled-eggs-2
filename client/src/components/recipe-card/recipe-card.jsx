76310186a136e800aea15ef4cf5d00847e8d7f08
valeravarchak <valeraavarchak@gmail.com>
2019-12-28 00:54:49 +0200

    fix img responsive

--- client/src/components/recipe-card/recipe-card.jsx
+++ client/src/components/recipe-card/recipe-card.jsx
@@ -19,15 +19,15 @@ class RecipeCard extends Component {
         let imageUrl = image;
 
         if (image) {
-            imageUrl = image.replace(/(.*)(uploads)\/(.*)$/, "./$2/$3");
+            imageUrl = image.replace(/(.*)(uploads)\/(.*)$/, "http://192.168.1.3:3000/$2/$3");
         } else {
-            imageUrl = "./uploads/notaviable.jpg";
+            imageUrl = "http://192.168.1.3:3000/uploads/notaviable.jpg";
         }
 
         return (
             <div className="col-xl-3 col-lg-4 col-sm-6 col-12">
                 <div className="card recipe-card">
-                    <div className="embed-responsive embed-responsive-16by9">
+                    <div className="embed-responsive embed-responsive-4by3">
                         <img src={imageUrl} alt="q" className="recipe-image embed-responsive-item" />
                     </div>
                     <div className="card-body">
