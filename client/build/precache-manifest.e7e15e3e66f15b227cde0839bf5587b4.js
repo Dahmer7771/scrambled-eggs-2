e6b8d18129bcafda81979cb102205080c8fdf912
dahmer7771 <Grichenko122@gmail.com>
2019-12-27 18:55:48 +0300

    merge

---
+++ client/build/precache-manifest.e7e15e3e66f15b227cde0839bf5587b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+    {
+        revision: "0c13d5910ac1a4ea84e48a56d9c2f1c9",
+        url: "/index.html",
+    },
+    {
+        revision: "f54a42eabb4c3204fbf9",
+        url: "/static/css/2.3d86be27.chunk.css",
+    },
+    {
+        revision: "7aa41d17ea62038ab62f",
+        url: "/static/css/main.ad3c1717.chunk.css",
+    },
+    {
+        revision: "f54a42eabb4c3204fbf9",
+        url: "/static/js/2.8b97b969.chunk.js",
+    },
+    {
+        revision: "d35bb95acbf4d4ae17c99face9498f03",
+        url: "/static/js/2.8b97b969.chunk.js.LICENSE",
+    },
+    {
+        revision: "7aa41d17ea62038ab62f",
+        url: "/static/js/main.73c92ed2.chunk.js",
+    },
+    {
+        revision: "42563c0706ebf2262a23",
+        url: "/static/js/runtime-main.e6d8a004.js",
+    },
+]);
