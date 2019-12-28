cd6b21a7177a7650324b99a37b8f71cd1d59578f
Nik <nikitaholub1999@gmail.com>
2019-12-28 03:29:04 +0200

    new build2

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 
 const PORT = process.env.PORT || 80;
-const DNS = "g5.sumdu-tss.site";
+const DNS = "http://g5.sumdu-tss.site";
 
 app.use("", recipeRoutes);
 app.use("", userRoutes);
