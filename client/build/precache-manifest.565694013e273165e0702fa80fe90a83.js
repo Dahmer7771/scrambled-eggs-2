c0851b6185d35c11e128a12ddace5187d7c3c5f7
dahmer7771 <Grichenko122@gmail.com>
2020-01-10 02:52:35 +0200

    build

---
+++ client/build/precache-manifest.565694013e273165e0702fa80fe90a83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "804397d9756e0f380869b42f719c8dc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f3dccf51eccbecbbb8",
+    "url": "/static/css/2.3d86be27.chunk.css"
+  },
+  {
+    "revision": "a5ca344b0f05b2ce3636",
+    "url": "/static/css/main.fbb2dd82.chunk.css"
+  },
+  {
+    "revision": "27f3dccf51eccbecbbb8",
+    "url": "/static/js/2.0ed62368.chunk.js"
+  },
+  {
+    "revision": "9f45d36ffa2ce789d47ad57c32d52617",
+    "url": "/static/js/2.0ed62368.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a5ca344b0f05b2ce3636",
+    "url": "/static/js/main.883c23e0.chunk.js"
+  },
+  {
+    "revision": "42563c0706ebf2262a23",
+    "url": "/static/js/runtime-main.e6d8a004.js"
+  }
+]);
